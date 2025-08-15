4cbb9d1a3c0f0c524ac31d814576c48474bdf47b
SoheilHosseini <canopus.soheilhosseini@gmail.com>
2025-08-15 11:32:11 -0700

    complete the header

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.rtl.min.css';
+import { ThemeProvider } from 'react-bootstrap';
+import "./style.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <ThemeProvider dir="rtl">
+    <App />
+  </ThemeProvider>
+);
