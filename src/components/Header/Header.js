7efa363581acd0b42b83036b10d212537b8547ed
SoheilHosseini <canopus.soheilhosseini@gmail.com>
2025-08-15 11:32:11 -0700

    complete the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,22 @@
 import "./Header.css"
 import profile from "../../assets/images/profile.jpg"
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
 
-function Header(){
-    return(
-        <img src={profile}/>
+function Header() {
+    return (
+        <div className="mt-5">
+            <Container>
+                <Row className="d-flex justify-content-center">
+                    <Col xs={12} className="text-center">
+                        <Image src={profile} roundedCircle />
+                        <p className="mt-4">بچه های مسجد اباعبدالله الحسین</p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 }
 
