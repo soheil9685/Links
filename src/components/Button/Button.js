2bb6f247b51effa687b505f907c0fed2041a81b2
SoheilHosseini <canopus.soheilhosseini@gmail.com>
2025-08-15 13:04:34 -0700

    Complete the links

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,20 @@
-function Button(){
-    return(
-        <></>
+import Nav from 'react-bootstrap/Nav';
+
+function Button({ icon, text, href }) {
+    return (
+        <Nav.Link href={href} className='m-3 d-flex p-2 rounded'
+            style={{
+                backgroundColor: "#11999E",
+                fontSize: "20px",
+                color: "#E4F9F5",
+                boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px"
+            }}>
+            <i className={icon} style={{
+                fontSize: "30px",
+                marginLeft : "15px",
+            }}></i>
+            {text}
+        </Nav.Link>
     )
 }
 
